5c09925bdfd15b0b6ddb4046151e5b71923d9a4d
cake <Wanphen9211@gmail.com>
2023-12-02 20:58:11 +0700

    promotion

--- src/page/Promotion.jsx
+++ src/page/Promotion.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Nav from '../navigation/Nav';
 import { collection, collectionGroup, getDocs, query } from 'firebase/firestore';
 import { firestore } from '../database/firebase';
+import '../pagecss/Promotion.css'
 
 function Promotion() {
   const [profiles, setProfiles] = useState([]);
@@ -26,16 +27,39 @@ function Promotion() {
   return (
     <>
       <Nav />
-      <div className='box-Promotion'>
-        <div className="box-Promotion-layer">
-          <div className="layer1">
-            <div className="layer1-box">โปรโมชันและสิทธิพิเศษ</div>
-          </div>
-          <div className="layer2">
-            <div className="layer1-box">โปรโมชันและสิทธิพิเศษ</div>
+      
+      
+      <div className="card">
+          <h2>โปรโมชันและสิทธิพิเศษ</h2>
+        </div>
+        
+      <div className='card-container'>
+        <div className='cards'>
+          <div className='image'> </div>
+          <div className='taitle'> <p>คูปองส่วนลด</p> </div>
+          <div className='des'>
+            <button>รับคูปอง</button> 
           </div>
         </div>
+
+        <div className='cards'>
+          <div className='image'> </div>
+          <div className='taitle'> <p>คูปองส่วนลด</p> </div>
+          <div className='des'>
+            <button>รับคูปอง</button> 
+          </div>
+        </div>
+
+        <div className='cards'>
+          <div className='image'> </div>
+          <div className='taitle'> <p>คูปองส่วนลด</p> </div>
+          <div className='des'>
+            <button>รับคูปอง</button> 
+          </div>
+        </div>  
+
       </div>
+      
     </>
   )
 }
