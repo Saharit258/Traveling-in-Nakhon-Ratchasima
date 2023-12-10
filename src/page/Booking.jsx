344f0d0602eccbba4b658329b856392a98587e5c
cake <Wanphen9211@gmail.com>
2023-12-11 00:06:23 +0700

    booking nidnoy

--- src/page/Booking.jsx
+++ src/page/Booking.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import Nav from '../navigation/Nav'
+import '../pagecss/Booking.css'
 
 function Booking() {
   return (
     <>
         <Nav />
-        <div>Booking</div>
+        <div >
+          
+        <div className="search-card-book">
+                        <div className="line-hotel">
+                            <p>ชื่อที่พัก/สถานที่</p>
+                            <input type="search" id="gsearch" className="gsearch-hotel"></input>
+                            </div>
+                        <div className="chack-in">
+                            <p>เช็คอิน</p>
+                            <input type="date" id="birthday" className="birthday"></input>
+                        </div>
+                        <div className="chack-out">                            
+                            <p>เช็คเอาท์</p>
+                            <input type="date" id="birthday" className="birthday"></input>
+                        </div>
+                        <div className="chack-person">
+                            <p>ผู้ใหญ่</p>
+                            <select className="cars" id="cars">
+                                <option value="volvo">1</option>
+                                <option value="saab">2</option>
+                                <option value="opel">3</option>
+                                <option value="audi">4</option>
+                            </select>
+                        </div>
+                        <div className="chack-sun">
+                        <p>เด็ก</p>
+                            <select className="cars" id="cars">
+                                <option value="volvo">1</option>
+                                <option value="saab">2</option>
+                                <option value="opel">3</option>
+                                <option value="audi">4</option>
+                            </select>
+                        </div>
+                        <div className="chack-button">
+                            <button>ตกลง</button>
+                        </div>
+                    </div>
+
+        </div>
     </>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -90,7 +90,49 @@ function Home() {
                         <span className="smoke-span">A</span>
                         <span className="smoke-span">T</span>
                     </div>
+
+                    <div className="search-card">
+                        <div className="line-hotel">
+                            <p>ชื่อที่พัก/สถานที่</p>
+                            <input type="search" id="gsearch" className="gsearch-hotel"></input>
+                            </div>
+                        <div className="chack-in">
+                            <p>เช็คอิน</p>
+                            <input type="date" id="birthday" className="birthday"></input>
+                        </div>
+                        <div className="chack-out">                            
+                            <p>เช็คเอาท์</p>
+                            <input type="date" id="birthday" className="birthday"></input>
+                        </div>
+                        <div className="chack-person">
+                            <p>ผู้ใหญ่</p>
+                            <select className="cars" id="cars">
+                                <option value="volvo">1</option>
+                                <option value="saab">2</option>
+                                <option value="opel">3</option>
+                                <option value="audi">4</option>
+                            </select>
+                        </div>
+                        <div className="chack-sun">
+                        <p>เด็ก</p>
+                            <select className="cars" id="cars">
+                                <option value="volvo">1</option>
+                                <option value="saab">2</option>
+                                <option value="opel">3</option>
+                                <option value="audi">4</option>
+                            </select>
+                        </div>
+                        <div className="chack-button">
+                            <button>ตกลง</button>
+                        </div>
+                    </div>
+
                 </div>
+
+
+                   
+
+
             <Cursor/>
         </div>
     )
